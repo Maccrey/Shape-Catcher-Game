7840b9ae5c28e36a058b7bbfc1db435f5e84d981
Maccrey <m01071630214@gmail.com>
2025-10-06 13:22:46 +0900

    feat(gameplay): reduce shape fall speed by additional 50% (0.6->0.3)

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -14,7 +14,7 @@ export const CATCHER_SPEED = 50;
 // Shape properties
 export const SHAPE_SIZE = 40;
 export const SHAPE_SPAWN_MARGIN = 20;
-export const SHAPE_FALL_SPEED_BASE = 0.6;
+export const SHAPE_FALL_SPEED_BASE = 0.3;
 
 // Combo system
 export const COMBO_TIMEOUT = 3000; // 3 seconds
